0c4735dc71cb50b55dcaac65eb2bb1c395d84ecf
MiddleTime <nadoroge@gmail.com>
2018-09-22 21:20:19 +0300

    Start!

---
+++ src/TestCloseMethod/Main.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TestCloseMethod;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ *
+ * @author inbox_bv8awe9
+ */
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> integers = Arrays.asList(1,2,3,4);
+        List<String> strings = Arrays.asList("bb", "a", "ccc");
+        
+        Integer[] a = {1,2,3};
+        
+        
+    }
+}
