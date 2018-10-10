5afd5351644c16df5f2ac54fb9580ae3d4c7c9c5
MiddleTime <nadoroge@gmail.com>
2018-10-10 13:51:16 +0300

    Testing

---
+++ src/thisTest/Dog.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package thisTest;
+
+/**
+ *
+ * @author inbox_bv8awe9
+ */
+public class Dog {
+    String name;
+    int age;
+
+    public Dog(String n, int a) {
+        //для того, чтобы продемонстрировать почему нужен this
+        name = name;
+        //this.name = n;
+        age = age;
+    }
+    
+    
+}
---
+++ src/thisTest/Main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package thisTest;
+
+/**
+ *
+ * @author inbox_bv8awe9
+ */
+public class Main {
+    public static void main(String[] args) {
+        Dog dog = new Dog("Reks", 5);
+        System.out.println(dog.name + " " + dog.age);
+        
+        
+    }
+}
