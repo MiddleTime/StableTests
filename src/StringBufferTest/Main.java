1f9f241c74fb0c0168c01c1b2255a53a6412bc54
MiddleTime <nadoroge@gmail.com>
2018-10-10 13:26:38 +0300

    Testing

---
+++ src/StringBufferTest/Main.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package StringBufferTest;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ *
+ * @author inbox_bv8awe9
+ */
+public class Main {
+    public static void main(String[] args) throws IOException {
+        //System.in - читаем поток байтов
+        //InputStreamReader обертка - считает символы
+        //bufferedReader - считывает строки
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+//        StringBuilder str2 = new StringBuilder(str); 
+//        System.out.println("You enter: " + str);
+//        System.out.println("But reverse form will be: " + str2.reverse());
+        
+        //а теперь посмотрим как работает System.in
+//        while(true){
+//            int byteValue = System.in.read();
+//            System.out.println(byteValue);  //некоторые полученные символы можно получить по компб Alt  +  номер))
+//            
+//        }
+        
+        InputStreamReader isr = new InputStreamReader(System.in);
+        for(;;){
+        int isrValue = isr.read();
+          System.out.println(isrValue); //считывается в один прием. произошло преобразование в UTF-16.  
+          System.out.println((char)isrValue);            
+        }
+        
+    }
+}
